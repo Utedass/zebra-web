4f894da5e303c8a69a414bb02f2d8da737888508
Jonatan Gezelius <jonatan.gezelius@etteplan.com>
2026-03-16 09:32:13 +0100

    Make Zebra page the start page. Add Shadcn to project.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,19 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { PageProps } from './$types';
+	import { Button } from '$lib/components/ui/button';
+	//let { form }: PageProps = $props();
+</script>
+
 <article class="prose">
-	<h1>Things will soon happen here!</h1>
-	<p>Another little change</p>
-	<p>Keep on tryin till you run out of cake</p>
-	<p>I think we won actually.</p>
+	<h1>Din etikettstation i webben</h1>
 </article>
+Den här texten då?
+
+<form method="POST" use:enhance>
+	<!--
+			<input type="string" class="input" name="stringJohan" />
+			<button type="submit" class="btn preset-filled">Tjoho!</button>
+			-->
+	<Button variant="outline" type="submit" aria-label="Submit">Frysdag</Button>
+</form>
