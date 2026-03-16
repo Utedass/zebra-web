fc7c9688a9154380d90328668084fa236389d3b0
Jonatan Gezelius <jonatan.gezelius@etteplan.com>
2026-03-16 14:36:47 +0100

    Change layout to card.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	//import type { PageProps } from './$types';
 	import { Button } from '$lib/components/ui/button';
 	import { Slider } from '$lib/components/ui/slider';
+	import * as Card from '$lib/components/ui/card';
 	//let { form }: PageProps = $props();
 
 	let qty = $state(1);
@@ -13,23 +14,33 @@
 	];
 </script>
 
-<article class="prose">
-	<h1>Din etikettstation i webben</h1>
-</article>
+<Card.Root class="mx-auto mt-2 max-w-60">
+	<Card.Header>
+		<Card.Title>Etikett med vett!</Card.Title>
+		<Card.Description>Din etikettstation i webben</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<div class="flex flex-col gap-2">
+			<p>
+				Antal etiketter: <span class="text-right font-bold">{qty}</span>
+			</p>
+			<Slider type="single" bind:value={qty} min={1} max={10} />
 
-<div class="flex-col max-w-50">
-	Antal etiketter: {qty}
-	<Slider type="single" bind:value={qty} min={1} max={10} />
-</div>
-
-{#each etiketter as etikett (etikett.label_id)}
-	<form method="POST" action="?/print" use:enhance>
-		<!--
+			<div class="flex flex-col gap-2">
+				{#each etiketter as etikett (etikett.label_id)}
+					<form method="POST" action="?/print" use:enhance>
+						<!--
 		<input type="string" class="input" name="stringJohan" />
 		<button type="submit" class="btn preset-filled">Tjoho!</button>
 		-->
-		<input type="hidden" name="label_id" value={etikett.label_id} />
-		<input type="hidden" name="qty" value={qty} />
-		<Button variant="outline" type="submit" aria-label="Submit">{etikett.display}</Button>
-	</form>
-{/each}
+						<input type="hidden" name="label_id" value={etikett.label_id} />
+						<input type="hidden" name="qty" value={qty} />
+						<Button class="w-full" variant="outline" type="submit" aria-label="Submit"
+							>{etikett.display}</Button
+						>
+					</form>
+				{/each}
+			</div>
+		</div>
+	</Card.Content>
+</Card.Root>
