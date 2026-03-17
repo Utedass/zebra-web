16a5f936888c138adb83e2a1cf7883218bec6b8e
Jonatan Gezelius <boolean.adapter@gmail.com>
2026-03-17 22:01:25 +0100

    Adjust display texts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,16 +8,19 @@
 
 	let qty = $state(1);
 
+	const now = new Date();
+	const timestring = now.toISOString().slice(0, 10); // "YYYY-MM-DD"
+
 	const etiketter = [
-		{ display: 'Frysdag', label_id: 'frysdag' },
-		{ display: 'Dagens datum bara', label_id: 'today' },
-		{ display: 'Jonatan + nummer', label_id: 'jonatan' },
-		{ display: 'Isabelle + nummer', label_id: 'isabelle' }
+		{ display: `Frysdag <br /> ${timestring}`, label_id: 'frysdag' },
+		{ display: `${timestring}`, label_id: 'today' },
+		{ display: 'Jonatan Gezelius <br /> +46 (0)73-58 48 690', label_id: 'jonatan' },
+		{ display: 'Isabelle Fägerman <br /> +46 (0)73-316 35 80', label_id: 'isabelle' }
 		//{ display: 'Experiment', label_id: 'experiment' }
 	];
 </script>
 
-<Card.Root class="mx-auto mt-2 max-w-60">
+<Card.Root class="mx-auto mt-2 max-w-80">
 	<Card.Header>
 		<Card.Title>Etikett med vett!</Card.Title>
 		<Card.Description>Din etikettstation i webben</Card.Description>
@@ -27,7 +30,7 @@
 			<p>
 				Antal etiketter: <span class="text-right font-bold">{qty}</span>
 			</p>
-			<Slider class="" type="single" bind:value={qty} min={1} max={10} />
+			<Slider class="h-8" type="single" bind:value={qty} min={1} max={10} />
 
 			<div class="flex flex-col gap-2">
 				{#each etiketter as etikett (etikett.label_id)}
@@ -38,8 +41,8 @@
 		-->
 						<input type="hidden" name="label_id" value={etikett.label_id} />
 						<input type="hidden" name="qty" value={qty} />
-						<Button class="w-full" variant="outline" type="submit" aria-label="Submit"
-							>{etikett.display}</Button
+						<Button class="w-full py-7" variant="outline" type="submit" aria-label="Submit"
+							>{@html etikett.display}</Button
 						>
 					</form>
 				{/each}
